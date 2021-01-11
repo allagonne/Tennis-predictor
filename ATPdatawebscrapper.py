10442c898206a27e36605c34d4c1aff40a664616
allagonne <allagonne@users.noreply.github.com>
2021-01-11 17:54:12 +0100

    Update ATPdatawebscrapper.py

--- ATPdatawebscrapper.py
+++ ATPdatawebscrapper.py
@@ -1,2 +1,8 @@
-## this is a local test file
+## Data webscraping from ATP website : https://www.atptour.com/
+from selenium import webdriver
 
+Tennis_data_collection = open('Tennis_player_details_scrape.csv', 'w')
+
+Tennis_data_collection.write('Player' + ',' + 'Career_length'+ ',' +  'Aces' + ',' + 'Double_faults' + ',' + 'First_serve' + ',' + '1st_Serve_Points_Won'
+                             + ',' + '2nd_Serve_Points_Won' + ',' + 'Break_Points_Faced' + ',' + 'Break_Points_Saved' + ',' + 'Service_Games_Played' + ',' +
+                             'Service_Games_Won' + ',' + 'Total_Service_Points_Won' + ',' + '\n')
